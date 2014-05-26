28d4bb164ac0ee6d58dac4131f959ad3fbbab227
rafacc <rafacc@IBM-87e6f2687ab>
2014-05-26 09:47:16 -0300

    Imporatação do XML

--- UNAHelpDeskGI/src/java/com/br/helpdeskgi/controller/IncidenteController.java
+++ UNAHelpDeskGI/src/java/com/br/helpdeskgi/controller/IncidenteController.java
@@ -50,10 +50,27 @@ public class IncidenteController {
         incidenteDao = new IncidenteDao();
         return incidenteDao.listarSolicitantes();
     }
-    
+
     public void handleAtendChange() {
         if (incidente.getCategoria() != 0) {
             //getAtendentes(incidente.getCategoria());
         }
+    }
+
+    public String criarIncidente2() {
+        incidenteDao = new IncidenteDao();
+        Incidente incidentexml = new Incidente();
+        incidentexml.setIdChamado("456a819d-b3b5-4cf6-afa1-8f12804e2a42");
+        incidentexml.setCategoria(6);
+        incidentexml.setPrioridade(4);
+        incidentexml.setDb_solicitante_id(21);
+        incidentexml.setDb_atendente_id(14);
+        incidentexml.setDescricao("Favor verificar a construção de DataMarchs para a solução proposta. Contato:3183221212");
+        incidentexml.setEscalonamento(4);
+        incidentexml.setStatus(0);
+        incidenteDao.salvar(incidentexml);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Incidente Armazenado", "Incidente Armazenado"));
+        incidente = new Incidente();
+        return null;
     }
 }
